9c8907cc5bf22aaa202763b72f03c78b3bda9fa6
Nicolai Ort <info@nicolai-ort.com>
2021-04-24 20:47:00 +0200

    Changed the method of svg linking

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -28,33 +28,50 @@
 				dark:text-gray-400 border-b dark:border-gray-600"
 	>
 		<!-- Header -->
-		<div>
-			<span>
-				<span class="text-green-500 dark:text-green-200"> 23 </span>
-				Different coins on bitpanda
-			</span>
-			<span>
-				<span class="text-green-500 dark:text-green-200"> 22 </span>
-				Other wallets
-			</span>
-			<span>
-				<span class="text-green-500 dark:text-green-200"> 33 </span>
-				roles
-			</span>
-		</div>
+		{#await promises[0] && promises[1]}
+			<p>Loading data....</p>
+		{:then}
+			<div>
+				<span>
+					<span class="text-green-500 dark:text-green-200"> {current_wallets.length} </span>
+					Coins on bitpanda
+				</span>
+				<span>
+					<span class="text-green-500 dark:text-green-200"> {current_indices.length} </span>
+					Indices on bitpanda
+				</span>
+				<span>
+					<span class="text-green-500 dark:text-green-200"> 22 </span>
+					Other wallets
+				</span>
+			</div>
+		{/await}
 	</div>
 	<div class="mt-4">
 		{#await promises[0] && promises[1]}
 			<p>Loading data....</p>
 		{:then}
 			<Statscard
-				title="Bitpanda Index"
-				value={current_indices.reduce((sum, cur) => (sum = sum + cur.fiat), 0).toFixed(2)}
-			/>
-			<Statscard
 				title="Bitpanda Tokens"
 				value={current_wallets.reduce((sum, cur) => (sum = sum + cur.fiat), 0).toFixed(2)}
-			/>
+				><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"
+					><path fill="none" d="M0 0h24v24H0z" /><path
+						fill="currentColor"
+						d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 100-16 8 8 0 000 16zm-1-4H8V8h3V6h2v2h1a2.5 2.5 0 012 4 2.5 2.5 0 01-2 4h-1v2h-2v-2zm-1-3v1h4a.5.5 0 100-1h-4zm0-3v1h4a.5.5 0 100-1h-4z"
+					/></svg
+				>
+			</Statscard>
+			<Statscard
+				title="Bitpanda Index"
+				value={current_indices.reduce((sum, cur) => (sum = sum + cur.fiat), 0).toFixed(2)}
+			>
+				<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"
+					><path fill="none" d="M0 0h24v24H0z" /><path
+						fill="currentColor"
+						d="M4 5v14h16V5H4zM3 3h18a1 1 0 011 1v16a1 1 0 01-1 1H3a1 1 0 01-1-1V4a1 1 0 011-1zm11.793 6.793L13 8h5v5l-1.793-1.793-3.864 3.864-2.121-2.121-2.829 2.828-1.414-1.414 4.243-4.243 2.121 2.122 2.45-2.45z"
+					/></svg
+				></Statscard
+			>
 		{/await}
 	</div>
 </div>
