824a2c30712bb4e938f1643a387398c65dd9df59
Nicolai Ort <info@nicolai-ort.com>
2021-04-24 20:57:55 +0200

    Removed useless comments

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,10 +1,9 @@
 const sveltePreprocess = require('svelte-preprocess');
 
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: sveltePreprocess({
 		postcss: true
 	}),
