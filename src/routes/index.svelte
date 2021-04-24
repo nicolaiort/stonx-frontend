cd9bdb3f109baff8bf0996a6767815a941a23176
Nicolai Ort <info@nicolai-ort.com>
2021-04-24 20:36:39 +0200

    Now with working dashboard cards for bitpanda indices and tokens

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
 	import Statscard from '$lib/Statscard.svelte';
+	import axios from 'axios';
+
+	const baseurl = 'http://localhost:8083';
+	$: current_indices = [];
+	$: current_wallets = [];
+
+	let promises: Promise<any>[] = new Array<Promise<any>>();
+
+	promises.push(
+		axios.get(`${baseurl}/rest/bitpanda/assets/index`).then((res) => {
+			current_indices = res.data;
+		})
+	);
+	promises.push(
+		axios.get(`${baseurl}/rest/bitpanda/assets/crypto`).then((res) => {
+			current_wallets = res.data;
+		})
+	);
 </script>
 
 <div class="mx-10 my-2">
@@ -24,31 +42,19 @@
 				roles
 			</span>
 		</div>
-		<!-- <div>
-			<span class="capitalize">
-				project
-				<span class="text-green-500 dark:text-green-200
-                    cursor-pointer">
-					all
-				</span>
-			</span>
-			<span class="capitalize ml-12">
-				date added
-				<span class="text-green-500 dark:text-green-200
-                    cursor-pointer">
-					all time
-				</span>
-			</span>
-			<span class="capitalize ml-12">
-				role
-				<span class="text-green-500 dark:text-green-200
-                    cursor-pointer">
-					all
-				</span>
-			</span>
-		</div> -->
 	</div>
 	<div class="mt-4">
-		<Statscard title="Bitpanda" value="3€" />
+		{#await promises[0] && promises[1]}
+			<p>Loading data....</p>
+		{:then}
+			<Statscard
+				title="Bitpanda Index"
+				value={current_indices.reduce((sum, cur) => (sum = sum + cur.fiat), 0).toFixed(2)}
+			/>
+			<Statscard
+				title="Bitpanda Tokens"
+				value={current_wallets.reduce((sum, cur) => (sum = sum + cur.fiat), 0).toFixed(2)}
+			/>
+		{/await}
 	</div>
 </div>
