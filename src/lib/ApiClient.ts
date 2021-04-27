87231f03da3e2f2d17935feb70113c9cab680aa7
Nicolai Ort <info@nicolai-ort.com>
2021-04-27 20:02:00 +0200

    Added auth header

--- src/lib/ApiClient.ts
+++ src/lib/ApiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const baseurl = 'http://localhost:8083';
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhY2NvdW50cy5leGFtcGxlc29mdC5jb20iLCJhdWQiOiJ5b3Vyc2l0ZS5uZXQiLCJzdWIiOiI4Nzc0NzdmOC1jZGI1LTRhZDYtYmUwZS02MmY0OTZkN2E3YjAiLCJleHAiOjE2MTk1NDk2NTEzMjQsImlhdCI6MTYxOTU0NjA1MTMyNH0.Guxt8KTipTExYMB_rX_BF8I7Yc-qbroAkWeumusIFpw"
 const axiosoptions = {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}}` }
 }
 
 export class ApiClient {
