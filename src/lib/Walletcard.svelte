5c1b75348534a6659100d9c8c9e023995f9efe54
Nicolai Ort <info@nicolai-ort.com>
2021-04-28 19:36:43 +0200

    Added basic walletcard to bitpanda overview

---
+++ src/lib/Walletcard.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	export let coin_name: string = 'BTC';
+	export let coin_amount: number = 420.69;
+	export let coin_fiat: number = 69;
+</script>
+
+<div class="mt-8 flex px-4 py-4 justify-between bg-white dark:bg-gray-600 shadow-xl rounded-lg">
+	<!-- Card -->
+
+	<div class="flex justify-between">
+		<!-- Left side -->
+
+		<span class="h-12 w-12 rounded-full object-cover">TODO:</span>
+
+		<div class="ml-4 flex flex-col capitalize text-gray-600 dark:text-gray-400">
+			<span>Token name</span>
+			<span class="mt-2 text-black dark:text-gray-200"> {coin_name} </span>
+		</div>
+
+		<div class="ml-12 flex flex-col capitalize text-gray-600 dark:text-gray-400">
+			<span>Current Amount</span>
+			<span class="mt-2 text-black dark:text-gray-200"> {coin_amount} </span>
+		</div>
+		<div class="ml-12 flex flex-col capitalize text-gray-600 dark:text-gray-400">
+			<span>Current Value(Fiat)</span>
+			<span class="mt-2 text-black dark:text-gray-200"> {coin_fiat.toFixed(2)} €</span>
+		</div>
+	</div>
+
+	<div class="flex">
+		<!-- Rigt side -->
+
+		<div class="mr-16 flex flex-col capitalize text-gray-600 dark:text-gray-400">
+			<span>project</span>
+			<span class="mt-2 text-black dark:text-gray-200"> Aero treck </span>
+		</div>
+
+		<div class="mr-16 flex flex-col capitalize text-gray-600 dark:text-gray-400">
+			<span>role</span>
+			<span class="mt-2 text-black dark:text-gray-200"> Front-End </span>
+		</div>
+
+		<div class="mr-16 flex flex-col capitalize text-gray-600 dark:text-gray-400">
+			<span>status</span>
+			<span class="mt-2 text-yellow-600 dark:text-yellow-400"> in work </span>
+		</div>
+
+		<div class="mr-8 flex flex-col capitalize text-gray-600 dark:text-gray-400">
+			<span>final date</span>
+			<span class="mt-2 text-green-400 dark:text-green-200"> 20.02.2020 11:00 </span>
+		</div>
+	</div>
+</div>
---
+++ src/routes/bitpanda.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { ApiClient } from '$lib/ApiClient';
+	import Walletcard from '$lib/Walletcard.svelte';
+
+	$: current_indices = [];
+	$: current_wallets = [];
+
+	let promises: Promise<any>[] = new Array<Promise<any>>();
+
+	promises.push(
+		ApiClient.getIndices().then((res) => {
+			current_indices = res;
+		})
+	);
+	promises.push(
+		ApiClient.getCrypto().then((res) => {
+			current_wallets = res;
+		})
+	);
+</script>
+
+<div class="mx-10 my-2">
+	<h2 class="my-4 text-4xl font-semibold dark:text-gray-400">Bitpanda</h2>
+
+	<div
+		class="pb-2 flex items-center justify-between text-gray-600
+				dark:text-gray-400 border-b dark:border-gray-600"
+	>
+		<!-- Header -->
+		{#await promises[0] && promises[1]}
+			<p>Loading data....</p>
+		{:then}
+			<div>
+				<span>
+					<span class="text-green-500 dark:text-green-200"> {current_wallets.length} </span>
+					Coins on bitpanda
+				</span>
+				<span>
+					<span class="text-green-500 dark:text-green-200"> {current_indices.length} </span>
+					Indices on bitpanda
+				</span>
+			</div>
+		{/await}
+	</div>
+	<div class="mt-4">
+		{#await promises[0] && promises[1]}
+			<p>Loading data....</p>
+		{:then}
+			{#each current_wallets as wallet}
+				<Walletcard
+					bind:coin_name={wallet.token}
+					bind:coin_amount={wallet.balance}
+					bind:coin_fiat={wallet.fiat}
+				/>
+			{/each}
+			<hr />
+		{/await}
+	</div>
+</div>
