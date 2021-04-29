3f634f50450e7d0b530af1d33e375bd50c3ffea9
Nicolai Ort <info@nicolai-ort.com>
2021-04-29 17:03:36 +0200

    USe previously implemented function

--- src/lib/modals/AddWalletModal.svelte
+++ src/lib/modals/AddWalletModal.svelte
@@ -119,7 +119,7 @@
 						</button>
 						<button
 							on:click={() => {
-								modal_open = false;
+								close();
 							}}
 							type="button"
 							class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
