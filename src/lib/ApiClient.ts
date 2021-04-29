80382f00a384f90e7f167bc796a41041ece7a54c
Nicolai Ort <info@nicolai-ort.com>
2021-04-29 15:22:43 +0200

    Now logging users out when the api returns a 401

--- src/lib/ApiClient.ts
+++ src/lib/ApiClient.ts
@@ -2,6 +2,15 @@ import axios from 'axios';
 import UserStore from './UserStore';
 let baseurl: string = 'http://localhost:8083';
 
+axios.interceptors.response.use(response => {
+    return response;
+}, error => {
+    if (error.response.status === 401) {
+        UserStore.logout();
+    }
+    return error;
+});
+
 export class ApiClient {
     static async getBitpandaCrypto(): Promise<any> {
         const res = await axios.get(`${baseurl}/rest/bitpanda/assets/crypto`, {
