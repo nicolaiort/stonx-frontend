565025c84c44ebbff8652e6aeb34e2eed1cab2d3
Nicolai Ort <info@nicolai-ort.com>
2021-04-29 16:21:53 +0200

    Implemented new wallet token selection

--- src/lib/AddWalletModal.svelte
+++ src/lib/AddWalletModal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { ApiClient } from './ApiClient';
+	import Select from 'svelte-select';
 
 	export let modal_open = true;
 	function focus(el) {
@@ -9,65 +10,95 @@
 	$: wallet_address = '';
 	$: wallet_token = '';
 	$: createbtnenabled = wallet_address != '' && wallet_token != '';
+	$: supported_tokens = [];
+
+	let token_promise = ApiClient.getSupportedTokens().then((result) => {
+		supported_tokens = result;
+	});
+
 	function submit() {
 		if (processed_last_submit === true) {
 			processed_last_submit = false;
 			ApiClient.createWallet(wallet_address, wallet_token);
+			console.log(wallet_token);
 		}
 	}
 </script>
 
 {#if modal_open}
-	<div class="fixed z-10 inset-0 overflow-y-auto">
-		<div
-			class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-		>
-			<div class="fixed inset-0 transition-opacity" aria-hidden="true">
-				<div class="absolute inset-0 bg-gray-500 opacity-75" data-id="modal_backdrop" />
-			</div>
-			<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true"
-				>&#8203;</span
-			>
+	{#await token_promise}
+		Loading supported tokens...
+	{:then}
+		<div class="fixed z-10 inset-0 overflow-y-auto">
 			<div
-				class="inline-block align-bottom bg-white dark:bg-gray-600 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-				role="dialog"
-				aria-modal="true"
-				aria-labelledby="modal-headline"
+				class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
 			>
-				<div class="bg-white dark:bg-gray-600 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-					<div class="sm:flex sm:items-start">
-						<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-							<h3 class="text-lg leading-6 font-medium text-black dark:text-gray-200">
-								Create a new wallet
-							</h3>
-							<div class="mt-2 mb-6">
-								<p class="text-sm text-black dark:text-gray-200">TODO:TExt</p>
-							</div>
-							<div class="grid grid-cols-6 gap-6" />
-						</div>
-					</div>
+				<div class="fixed inset-0 transition-opacity" aria-hidden="true">
+					<div class="absolute inset-0 bg-gray-500 opacity-75" data-id="modal_backdrop" />
 				</div>
-				<div class="bg-white dark:bg-gray-600 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-					<button
-						disabled={!createbtnenabled}
-						class:opacity-50={!createbtnenabled}
-						on:click={submit}
-						type="button"
-						class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-					>
-						Create
-					</button>
-					<button
-						on:click={() => {
-							modal_open = false;
-						}}
-						type="button"
-						class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-					>
-						Cancel
-					</button>
+				<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true"
+					>&#8203;</span
+				>
+				<div
+					class="inline-block align-bottom bg-white dark:bg-gray-600 rounded-lg text-left shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+					role="dialog"
+					aria-modal="true"
+					aria-labelledby="modal-headline"
+				>
+					<div class="bg-white dark:bg-gray-600 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+						<div class="sm:flex sm:items-start">
+							<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+								<h3 class="text-lg leading-6 font-medium text-black dark:text-gray-200">
+									Create a new wallet
+								</h3>
+								<div class="mt-2 mb-6">
+									<p class="text-sm text-black dark:text-gray-200">
+										Just pick from the supported tokens and enter your wallet's address (warning: we
+										don't verify them)
+									</p>
+								</div>
+								<div class="grid grid-cols-6 gap-6">
+									<div class="col-span-6">
+										<label
+											for="token"
+											class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+											>Token</label
+										>
+										<Select
+											containerClasses="rounded-l-md mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm rounded-l-md sm:text-sm border-gray-300 border bg-gray-50 dark:bg-grey-600 text-gray-500 dark:text-grey-300 rounded-md p-2"
+											bind:items={supported_tokens}
+											on:select={(selectedValue) => (wallet_token = selectedValue.detail.value)}
+											showChevron={true}
+											MultiSelection={false}
+										/>
+									</div>
+								</div>
+							</div>
+						</div>
+						{wallet_token}
+					</div>
+					<div class="bg-white dark:bg-gray-600 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+						<button
+							disabled={!createbtnenabled}
+							class:opacity-50={!createbtnenabled}
+							on:click={submit}
+							type="button"
+							class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+						>
+							Create
+						</button>
+						<button
+							on:click={() => {
+								modal_open = false;
+							}}
+							type="button"
+							class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+						>
+							Cancel
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	{/await}
 {/if}
