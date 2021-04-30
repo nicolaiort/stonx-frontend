34433a85640d3948cba90e95382337f30337de81
Nicolai Ort <info@nicolai-ort.com>
2021-04-30 20:04:32 +0200

    Fixed statscard styling

--- src/lib/Statscard.svelte
+++ src/lib/Statscard.svelte
@@ -5,12 +5,10 @@
 
 <div class="widget ml-2 p-4 rounded-lg bg-white dark:bg-gray-600 dark:text-gray-200 inline-block">
 	<div class="flex flex-row items-center justify-between">
-		<div class="flex flex-col">
+		<div class="flex flex-col mr-2">
 			<div class="text-xs uppercase font-light">{title}</div>
 			<div class="text-xl font-bold">{value}</div>
 		</div>
-		<div class="ml-3">
-			<slot />
-		</div>
+		<slot />
 	</div>
 </div>
