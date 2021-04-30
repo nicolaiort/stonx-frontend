4f1c488c46d3f191373d6a877ec40e31a00e1e0c
Nicolai Ort <info@nicolai-ort.com>
2021-04-30 20:50:17 +0200

    Added signup link to login

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -82,13 +82,13 @@
 				</button>
 			</div>
 		</div>
-		<!-- <div class="mt-2">
-					<a
-						href="/forgot_password"
-						class="block w-full text-center py-2 px-3 border border-gray-300 rounded-md font-medium hover:border-gray-400 focus:outline-none focus:border-gray-400 sm:text-sm"
-					>
-						{$_('forgot_password')}
-					</a>
-				</div> -->
+		<div class="mt-2">
+			<a
+				href="/register"
+				class="block w-full text-center dark:text-white py-2 px-3 border border-gray-300 rounded-md font-medium hover:border-gray-400 focus:outline-none focus:border-gray-400 sm:text-sm"
+			>
+				Sign Up
+			</a>
+		</div>
 	</div>
 </div>
