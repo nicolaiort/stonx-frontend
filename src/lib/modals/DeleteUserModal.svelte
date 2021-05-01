7cdf008089250067e0f78222a2658415d1a2bb07
Nicolai Ort <info@nicolai-ort.com>
2021-05-01 16:07:01 +0200

    Added user deletion modal

---
+++ src/lib/modals/DeleteUserModal.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { ApiClient } from '$lib/ApiClient';
+	import UserStore from '$lib/UserStore';
+	export let modal_open = true;
+
+	function submit() {
+		ApiClient.deleteMe(true).then((result) => {
+			close();
+			UserStore.logout();
+		});
+	}
+
+	function close() {
+		modal_open = false;
+	}
+</script>
+
+{#if modal_open}
+	<div class="fixed z-10 inset-0 overflow-y-auto">
+		<div
+			class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+		>
+			<div class="fixed inset-0 transition-opacity" aria-hidden="true">
+				<div class="absolute inset-0 bg-gray-500 opacity-75" data-id="modal_backdrop" />
+			</div>
+			<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true"
+				>&#8203;</span
+			>
+			<div
+				class="inline-block align-bottom bg-white dark:bg-gray-600 rounded-lg text-left shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+				role="dialog"
+				aria-modal="true"
+				aria-labelledby="modal-headline"
+			>
+				<div class="bg-white dark:bg-gray-600 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+					<div class="sm:flex sm:items-start">
+						<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+							<h3 class="text-lg leading-6 font-medium text-black dark:text-gray-200">
+								Profile deletion
+							</h3>
+							<div class="mt-2 mb-6">
+								<p class="text-sm text-black dark:text-gray-200">
+									Deletes your profile along all of the wallets you added from this service! No,
+									deleting wallets here doesn't delete them anywhere else ;)
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="bg-white dark:bg-gray-600 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+					<button
+						on:click={submit}
+						type="button"
+						class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+					>
+						Confirm, delete my profile
+					</button>
+					<button
+						on:click={close}
+						type="button"
+						class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+					>
+						Cancel, keep my profile
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+{/if}
