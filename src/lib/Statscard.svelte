a05d75a0e359cb608b8358ace72f7aea209718cc
Nicolai Ort <info@nicolai-ort.com>
2021-05-04 19:31:28 +0200

    Statscards now even more dynamic

--- src/lib/Statscard.svelte
+++ src/lib/Statscard.svelte
@@ -22,41 +22,38 @@
 					</div>
 
 					{#if change_percent}
-						{#if change_percent > 0}
-							<div class="ml-2 flex items-baseline text-sm font-semibold text-green-600">
-								<svg
-									class="self-center flex-shrink-0 h-5 w-5 text-green-500"
-									fill="currentColor"
-									viewBox="0 0 20 20"
-									aria-hidden="true"
-								>
+						<div
+							class:text-green-600={change_percent > 0}
+							class:text-red-600={change_percent < 0}
+							class="ml-2 flex items-baseline text-sm font-semibold"
+						>
+							<svg
+								class:text-green-500={change_percent > 0}
+								class:text-red-500={change_percent < 0}
+								class="self-center flex-shrink-0 h-5 w-5"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								aria-hidden="true"
+							>
+								{#if change_percent > 0}
 									<path
 										fill-rule="evenodd"
 										d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
 										clip-rule="evenodd"
 									/>
-								</svg>
-								<span class="sr-only"> Increased by </span>
-								{change_percent}%
-							</div>
-						{:else}
-							<div class="ml-2 flex items-baseline text-sm font-semibold text-red-600">
-								<svg
-									class="self-center flex-shrink-0 h-5 w-5 text-red-500"
-									fill="currentColor"
-									viewBox="0 0 20 20"
-									aria-hidden="true"
-								>
+								{:else if change_percent < 0}
 									<path
 										fill-rule="evenodd"
 										d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z"
 										clip-rule="evenodd"
 									/>
-								</svg>
-								<span class="sr-only"> Decreased by </span>
-								{change_percent}%
-							</div>
-						{/if}
+								{/if}
+							</svg>
+							<span class="sr-only">
+								{#if change_percent > 0}Increased by{:else if change_percent < 0}Decreased by{/if}
+							</span>
+							{change_percent}%
+						</div>
 					{/if}
 				</dd>
 			</div>
