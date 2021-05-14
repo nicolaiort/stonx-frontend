e7f352956a710f0e1a1bd672f8f76934c92483c6
Nicolai Ort <info@nicolai-ort.com>
2021-05-14 19:31:35 +0200

    Now only showing bitpanda related stuff when bitpanda is configured

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -60,23 +60,26 @@
 						/></svg
 					>
 				</Statscard>
-				<Statscard
-					title="Bitpanda Tokens"
-					link="/bitpanda"
-					link_text="Bitpanda assets"
-					value={bitpanda_wallets.reduce((sum, cur) => (sum = sum + cur.fiat), 0).toFixed(2) + ' €'}
-					><svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="text-white"
-						width="48"
-						height="48"
-						viewBox="0 0 24 24"
-						><path fill="none" d="M0 0h24v24H0z" /><path
-							fill="currentColor"
-							d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 100-16 8 8 0 000 16zm-1-4H8V8h3V6h2v2h1a2.5 2.5 0 012 4 2.5 2.5 0 01-2 4h-1v2h-2v-2zm-1-3v1h4a.5.5 0 100-1h-4zm0-3v1h4a.5.5 0 100-1h-4z"
-						/></svg
-					>
-				</Statscard>
+				{#if bitpanda_wallets.length > 0}
+					<Statscard
+						title="Bitpanda Tokens"
+						link="/bitpanda"
+						link_text="Bitpanda assets"
+						value={bitpanda_wallets.reduce((sum, cur) => (sum = sum + cur.fiat), 0).toFixed(2) +
+							' €'}
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="text-white"
+							width="48"
+							height="48"
+							viewBox="0 0 24 24"
+							><path fill="none" d="M0 0h24v24H0z" /><path
+								fill="currentColor"
+								d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 100-16 8 8 0 000 16zm-1-4H8V8h3V6h2v2h1a2.5 2.5 0 012 4 2.5 2.5 0 01-2 4h-1v2h-2v-2zm-1-3v1h4a.5.5 0 100-1h-4zm0-3v1h4a.5.5 0 100-1h-4z"
+							/></svg
+						>
+					</Statscard>
+				{/if}
 				{#if bitpanda_indices.length > 0}
 					<Statscard
 						title="Bitpanda Index"
