ccf5500ea0a291df6912e215de3cc3675aa3e296
Nicolai Ort <info@nicolai-ort.com>
2021-05-14 20:22:24 +0200

    Added current exchanges to settings

---
+++ src/lib/ExchangeCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let title: string = 'Title';
+</script>
+
+<div class="w-full h-full bg-white shadow rounded-lg">
+	<div class="px-4 py-5 sm:p-6">
+		<div class="items-center text-center">
+			<div class="rounded-md">
+				<slot />
+			</div>
+			<div class="mx-auto">
+				<dt class="text-sm font-medium text-gray-800">
+					{title}
+				</dt>
+			</div>
+		</div>
+	</div>
+</div>
