e488314cf01249d681709faf432738e8a777505d
Nicolai Ort <info@nicolai-ort.com>
2021-05-19 17:40:02 +0200

    Sidebar Item hoverstate

--- src/lib/base/SidebarItem.svelte
+++ src/lib/base/SidebarItem.svelte
@@ -6,7 +6,7 @@
 <!-- Current: "text-gray-300", Default: "text-gray-400 group-hover:text-gray-300" -->
 <a
 	href={link}
-	class="bg-gray-900 text-white group flex items-center px-2 py-2 mx-2 text-l font-medium rounded-md"
+	class="bg-gray-900 text-white hover:bg-black group flex items-center px-2 py-2 mx-2 text-l font-medium rounded-md"
 >
 	<slot />
 	{title}
