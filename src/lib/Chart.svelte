2ffd558d843ce2bef90ea153416a1a2e9a8fb54b
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 18:19:24 +0200

    Updated chart button styling

--- src/lib/Chart.svelte
+++ src/lib/Chart.svelte
@@ -34,17 +34,18 @@
 	<div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
 		<div class="max-w-2xl mx-auto">
 			<div class="bg-white overflow-hidden shadow rounded-lg">
-				<div class="px-4 py-5 sm:p-6">
+				<div class="px-4 pt-4 sm:p-6">
 					<Chart {data} type="line" class="w-full" />
 				</div>
-				<div class="border-t border-gray-200 px-4 py-4 sm:px-6">
-					<div class="p-8 bg-white flex items-center justify-center">
+				<div class="border-gray-200 px-4 py-4 sm:px-6">
+					<div class="bg-white flex items-center justify-center">
 						<span class="relative z-0 inline-flex shadow-sm rounded-md">
 							<button
 								type="button"
 								on:click={() => {
 									timespan = 'TODAY';
 								}}
+								class:bg-gray-100={timespan == 'TODAY'}
 								class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
 							>
 								Day
@@ -54,6 +55,7 @@
 								on:click={() => {
 									timespan = 'THISWEEK';
 								}}
+								class:bg-gray-100={timespan == 'THISWEEK'}
 								class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
 							>
 								Week
@@ -63,6 +65,7 @@
 								on:click={() => {
 									timespan = 'THISMONTH';
 								}}
+								class:bg-gray-100={timespan == 'THISMONTH'}
 								class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
 							>
 								Month
@@ -72,6 +75,7 @@
 								on:click={() => {
 									timespan = 'THISYEAR';
 								}}
+								class:bg-gray-100={timespan == 'THISYEAR'}
 								class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
 							>
 								Year
@@ -81,6 +85,7 @@
 								on:click={() => {
 									timespan = 'ALL';
 								}}
+								class:bg-gray-100={timespan == 'ALL'}
 								class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
 							>
 								Since start
