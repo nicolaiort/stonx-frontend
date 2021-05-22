0ed194b8abe8688c9e8b7a673492e01e951be7f8
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 18:34:30 +0200

    Now only showing buttons that can be used

--- src/lib/Chart.svelte
+++ src/lib/Chart.svelte
@@ -1,38 +1,57 @@
 <script>
 	import Chart from 'svelte-frappe-charts';
-	export let values = [];
-	export let timespan = 'TODAY';
+	export let values_fiat = {
+		day: [],
+		week: [],
+		month: [],
+		year: [],
+		all: []
+	};
 
-	$: data = getData(values, timespan);
+	$: timespan = 'TODAY';
+	$: data = getData(values_fiat, timespan);
 
 	function getData(values, timespan) {
+		let data;
+		switch (timespan) {
+			case 'TODAY':
+				data = values.day;
+				break;
+			case 'THISWEEK':
+				data = values.week;
+				break;
+			case 'THISMONTH':
+				data = values.month;
+				break;
+			case 'THISYEAR':
+				data = values.year;
+				break;
+			case 'ALL':
+				data = values.all;
+				break;
+			default:
+				throw new Error('Timeframe not supported');
+		}
+
 		return {
-			labels: getLabels(values, timespan),
+			labels: data.map((l) => {
+				if (timespan == 'TODAY') {
+					return new Date(l.timestamp).toLocaleTimeString();
+				}
+				return new Date(l.timestamp).toLocaleDateString();
+			}),
 			datasets: [
 				{
-					values: values.map((l) => l.fiat_value)
+					values: data.map((l) => l.fiat_value)
 				}
 			]
 		};
 	}
-
-	function getLabels(values, timespan) {
-		switch (timespan) {
-			case 'TODAY':
-				return values.map((l) => {
-					return new Date(l.timestamp).toLocaleTimeString();
-				});
-			default:
-				return values.map((l) => {
-					return new Date(l.timestamp).toLocaleDateString();
-				});
-		}
-	}
 </script>
 
 <div class="bg-gray-100">
-	<div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-		<div class="max-w-2xl mx-auto">
+	<div class="w-full mx-auto py-12 px-4 sm:px-6 lg:px-8">
+		<div class="mx-auto">
 			<div class="bg-white overflow-hidden shadow rounded-lg">
 				<div class="px-4 pt-4 sm:p-6">
 					<Chart {data} type="line" class="w-full" />
@@ -40,56 +59,66 @@
 				<div class="border-gray-200 px-4 py-4 sm:px-6">
 					<div class="bg-white flex items-center justify-center">
 						<span class="relative z-0 inline-flex shadow-sm rounded-md">
-							<button
-								type="button"
-								on:click={() => {
-									timespan = 'TODAY';
-								}}
-								class:bg-gray-100={timespan == 'TODAY'}
-								class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-							>
-								Day
-							</button>
-							<button
-								type="button"
-								on:click={() => {
-									timespan = 'THISWEEK';
-								}}
-								class:bg-gray-100={timespan == 'THISWEEK'}
-								class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-							>
-								Week
-							</button>
-							<button
-								type="button"
-								on:click={() => {
-									timespan = 'THISMONTH';
-								}}
-								class:bg-gray-100={timespan == 'THISMONTH'}
-								class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-							>
-								Month
-							</button>
-							<button
-								type="button"
-								on:click={() => {
-									timespan = 'THISYEAR';
-								}}
-								class:bg-gray-100={timespan == 'THISYEAR'}
-								class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-							>
-								Year
-							</button>
-							<button
-								type="button"
-								on:click={() => {
-									timespan = 'ALL';
-								}}
-								class:bg-gray-100={timespan == 'ALL'}
-								class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-							>
-								Since start
-							</button>
+							{#if values_fiat.day.length > 0}
+								<button
+									type="button"
+									on:click={() => {
+										timespan = 'TODAY';
+									}}
+									class:bg-gray-100={timespan == 'TODAY'}
+									class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+								>
+									Day
+								</button>
+							{/if}
+							{#if values_fiat.week.length > 0}
+								<button
+									type="button"
+									on:click={() => {
+										timespan = 'THISWEEK';
+									}}
+									class:bg-gray-100={timespan == 'THISWEEK'}
+									class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+								>
+									Week
+								</button>
+							{/if}
+							{#if values_fiat.week.month > 0}
+								<button
+									type="button"
+									on:click={() => {
+										timespan = 'THISMONTH';
+									}}
+									class:bg-gray-100={timespan == 'THISMONTH'}
+									class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+								>
+									Month
+								</button>
+							{/if}
+							{#if values_fiat.year.length > 0}
+								<button
+									type="button"
+									on:click={() => {
+										timespan = 'THISYEAR';
+									}}
+									class:bg-gray-100={timespan == 'THISYEAR'}
+									class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+								>
+									Year
+								</button>
+							{/if}
+							{#if values_fiat.all.length > 0}
+								<button
+									type="button"
+									on:click={() => {
+										timespan = 'ALL';
+									}}
+									class:bg-gray-100={timespan == 'ALL'}
+									class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+								>
+									Since start
+								</button>
+							{/if}
 						</span>
 					</div>
 				</div>
