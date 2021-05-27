d95d4b51cba930c50be0fba934b4bc3c6ef63034
Nicolai Ort <info@nicolai-ort.com>
2021-05-27 19:07:35 +0200

    Added new Doughnut-style diversity chart

---
+++ src/lib/DiversityChart.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Doughnut from '$lib/charts/Doughnut.svelte';
+	export let values = [];
+	export const responsive: boolean = false;
+
+	$: dataLine = getData(values);
+
+	function getData(values) {
+		return {
+			labels: values.map((l) => {
+				return l.token;
+			}),
+			datasets: [
+				{
+					data: values.map((l) => {
+						return l.fiat;
+					})
+				}
+			]
+		};
+	}
+</script>
+
+<div class="mx-auto w-full">
+	<div class="bg-white overflow-hidden shadow rounded-lg">
+		<div class="md:px-4 pt-4 sm:px-0 sm:py-6">
+			<Doughnut data={dataLine} options={{ responsive: true, maintainAspectRatio: responsive }} />
+		</div>
+	</div>
+</div>
